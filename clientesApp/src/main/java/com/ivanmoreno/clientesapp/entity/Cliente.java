9ee23704ad677001d62fb38c8c3544c349781e4c
ivan <ivanmoreno405@gmail.com>
2021-09-15 16:30:57 -0500

    first commit and Cliente entity was added

---
+++ clientesApp/src/main/java/com/ivanmoreno/clientesapp/entity/Cliente.java
@@ -0,0 +1,48 @@
+package com.ivanmoreno.clientesapp.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data // generates getters, setters, equals, toString & hashCode we well
+@Builder // generates builder for the fields within
+@NoArgsConstructor // generates a no argument constructor
+@AllArgsConstructor // generates a constructor with all arguments
+@ToString(exclude = {"posts"}) // generates toString method, skipping passed field as name
+@EqualsAndHashCode(exclude = {"posts"}) // generates equals and hashCode methods, skipping passed fields
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String email;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	
+	private static final long serialVersionUID = 1L;
+}
